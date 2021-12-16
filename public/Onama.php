71873023aca77d5d0da45d8235eaf9fbb5a4b932
unknown <david.milicic@fpmoz.sum.ba>
2021-12-16 18:10:28 +0100

    Dodan subsite u htaccess

--- public/Onama.php
+++ public/Onama.php
@@ -37,7 +37,7 @@ if (!empty($_GET['q'])) {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="/projekti/pzio/2022/g1/">Home</a>
+            <a class="nav-link" aria-current="page" href="{{ url('/') }}">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="Onama.php">O nama</a>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/projekti/pzio/2022/g1/', function () {
+Route::get('/', function () {
     return view('index');
 });
 
