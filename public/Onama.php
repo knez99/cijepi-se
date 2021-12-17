c41542c7a4dabb5a842b73d3b7704e7ad4bcde3a
unknown <david.milicic@fpmoz.sum.ba>
2021-12-17 13:25:33 +0100

    Promjenjene kartice i sad su responsive

--- public/Onama.php
+++ public/Onama.php
@@ -51,66 +51,62 @@ if (!empty($_GET['q'])) {
     </div>
   </nav>
 
+
   <!--Kartice tima-->
   <div class="container">
-    <div class="row">
-      <div class="cardteam">
-        <img src="images/david.png" class="card-img-top">
+    <div class="card-group">
+      <div class="card">
+        <img src="images/david.png" class="imgcard">
         <div class="card-body">
           <h5 class="card-title">David Miličić</h5>
-          <p class="card-text">Pozdrav, ja sam David Miličić. Imam 21 godinu i zivim u Ugljari. Student sam treće godine Informatike u Orašju. Slobodno vrijeme provodim igrajući igre i družeći se sa prijateljima.</p>
+          <p class="card-text">Pozdrav, ja sam David Miličić. Imam 21 godinu i živim u Ugljari. Student sam treće godine Informatike u Orašju. Slobodno vrijeme provodim igrajući igre i družeći se sa prijateljima.</p>
         </div>
       </div>
-
-
-
-      <div class="cardteam">
-        <img src="images/kruno.png" class="card-img-top">
+      <div class="card">
+        <img src="images/kruno.png" class="imgcard">
         <div class="card-body">
           <h5 class="card-title">Krunoslav Knežević</h5>
-          <p class="card-text">Pozdrav, ja sam Krunoslav Knezevic. Imam 22 godine i zivim u Jenjiću.Student sam treće godine Informatike u Orašju. U slobodno vrijeme igram nogomet,igre i družim se s prijateljima.</p>
+          <p class="card-text">Pozdrav, ja sam Krunoslav Knežević. Imam 22 godine i živim u Jenjiću. Student sam treće godine Informatike u Orašju. U slobodno vrijeme igram nogomet,igre i družim se s prijateljima.</p>
         </div>
       </div>
-
-
-
-      <div class="cardteam">
-        <img src="images/mario.jpg" class="card-img-top">
+      <div class="card">
+        <img src="images/mario.jpg" class="imgcard">
         <div class="card-body">
           <h5 class="card-title">Mario Nedić</h5>
-          <p class="card-text">Pozdrav, ja sam Mario Nedic. Imam 21 godinu i zivim u Tolisi. Trenutno sam student treće godine Informatike u Orašju. Uz faks, slobodno vrijeme provodim s prijateljima, s bratom se bavim izradom namještaja te vikednom radim u diskoteci. 😄</p>
+          <p class="card-text">Pozdrav, ja sam Mario Nedić. Imam 21 godinu i živim u Tolisi. Trenutno sam student treće godine Informatike u Orašju. Uz faks, slobodno vrijeme provodim s prijateljima, s bratom se bavim izradom namještaja te vikednom radim u diskoteci. 😄</p>
         </div>
       </div>
     </div>
+  </div>
 
-    <!--Kartice informacija-->
-    <div class="container">
-      <div class="card text-center">
-        <div class="card-body">
-          <h5 class="card-title">O projektu</h5>
-          <p class="card-text">Svrha dokumenta je omogućiti korisnicima stranice prijavu za vakciniranje. Bilo koji korisnik će se moći prijaviti na stranicu sapotrebnim dokumentima (osobna iskaznica) te se tako prijaviti za vakciniranje u bolnici. Radni naziv projekta je „Cijepi se“ i ono također potiče ljude na cijepljenje protiv virusa.</p>
-        </div>
+  <!--Kartice informacija-->
+  <div class="container">
+    <div class="card text-center">
+      <div class="card-body">
+        <h5 class="card-title">O projektu</h5>
+        <p class="card-text">Svrha dokumenta je omogućiti korisnicima stranice prijavu za vakciniranje. Bilo koji korisnik će se moći prijaviti na stranicu sapotrebnim dokumentima (osobna iskaznica) te se tako prijaviti za vakciniranje u bolnici. Radni naziv projekta je „Cijepi se“ i ono također potiče ljude na cijepljenje protiv virusa.</p>
       </div>
     </div>
-    <div class="container">
-      <div class="card text-center">
-        <div class="card-body">
-          <h5 class="card-title">Tehnologije</h5>
-          <p class="card-text">Koristit ćemo tehnologije kao: Laravel, Bootstrap 5 css i javascript, MySQL.</p>
-        </div>
+  </div>
+  <div class="container">
+    <div class="card text-center">
+      <div class="card-body">
+        <h5 class="card-title">Tehnologije</h5>
+        <p class="card-text">Koristit ćemo tehnologije kao: Laravel, Bootstrap 5 css i javascript, MySQL.</p>
       </div>
     </div>
-    <div class="container">
-      <div class="card text-center">
-        <div class="card-body">
-          <h5 class="card-title">Motivacija</h5>
-          <p class="card-text">Radimo ovo jer bi ova ideja jako pomogla u današnje vrijeme. Jako nas zanima kako bi mogli popraviti neke stvari u ovoj sredini, i cijepljenje je jedna od tih stvari. Ideja bi sigurno dobro prošla među našim ljudima jer olakšava cijepljenje.</p>
-        </div>
+  </div>
+  <div class="container">
+    <div class="card text-center">
+      <div class="card-body">
+        <h5 class="card-title">Motivacija</h5>
+        <p class="card-text">Radimo ovo jer bi ova ideja jako pomogla u današnje vrijeme. Jako nas zanima kako bi mogli popraviti neke stvari u ovoj sredini, i cijepljenje je jedna od tih stvari. Ideja bi sigurno dobro prošla među našim ljudima jer olakšava cijepljenje.</p>
       </div>
     </div>
+  </div>
 
-    <!-- Bootstrap JS with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <!-- Bootstrap JS with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
 
