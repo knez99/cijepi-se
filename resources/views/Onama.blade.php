615a9b8962ef15d45d0054d8171fbd3b0d90cb56
unknown <david.milicic@fpmoz.sum.ba>
2022-01-16 18:16:43 +0100

    Promjenjen izgled O nama

--- resources/views/Onama.blade.php
+++ resources/views/Onama.blade.php
@@ -22,7 +22,10 @@ if (!empty($_GET['q'])) {
   <link href="https://fonts.googleapis.com/css?family=Karla:400" rel="stylesheet" type="text/css">
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- Nas css -->
   <link rel="stylesheet" href="styles.css">
+  <!-- Za Bootstrap ikone -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 
 </head>
 
@@ -59,37 +62,60 @@ if (!empty($_GET['q'])) {
     </div>
   </nav>
 
+
+  <!-- Tim -->
   <div class="container">
     <div class="row">
       <div class="col-lg-4">
-      <img src="images/david.png" width="140" height="140">
-
-
-        <h2>David Miličić<</h2>
+        <img src="images/david.png" class="mx-auto d-block" width="140" height="140">
+        <h2 style="text-align: center;">David Miličić</h2>
         <p>Pozdrav, ja sam David Miličić. Imam 21 godinu i živim u Ugljari. Student sam treće godine Informatike u Orašju. Slobodno vrijeme provodim igrajući igre i družeći se sa prijateljima.</p>
-        <p><a class="btn btn-primary" href="#">Github</a></p>
+        <p><a class="btn btn-primary d-grid gap-2 col-6 mx-auto" href="https://github.com/DavidMilicic">Github</a></p>
       </div>
+
       <div class="col-lg-4">
-        <img src="images/kruno.png" width="140" height="140">
-
-
-        <h2>Krunoslav Knežević</h2>
-        <p>Pozdrav, ja sam Krunoslav Knežević. Imam 22 godine i živim u Jenjiću. Student sam treće godine Informatike u Orašju. U slobodno vrijeme igram nogomet,igre i družim se s prijateljima.</p>
-        <p><a class="btn btn-primary" href="#">Github</a></p>
+        <img src="images/kruno.png" class="mx-auto d-block" width="140" height="140">
+        <h2 style="text-align: center;">Krunoslav Knežević</h2>
+        <p style="text-align: center;">Pozdrav, ja sam Krunoslav Knežević. Imam 22 godine i živim u Jenjiću. Student sam treće godine Informatike u Orašju. U slobodno vrijeme igram nogomet,igre i družim se s prijateljima.</p>
+        <p><a class="btn btn-primary d-grid gap-2 col-6 mx-auto" href="https://github.com/knez99">Github</a></p>
       </div>
+      
       <div class="col-lg-4">
-      <img src="images/mario.png" width="140" height="140">
-
-
-        <h2>Mario Nedić</h2>
-        <p>Pozdrav, ja sam Mario Nedić. Imam 21 godinu i živim u Tolisi. Trenutno sam student treće godine Informatike u Orašju. Uz faks, slobodno vrijeme provodim s prijateljima, s bratom se bavim izradom namještaja te vikednom radim u diskoteci. 😄</p>
-        <p><a class="btn btn-primary" href="#">Github</a></p>
+        <img src="images/mario.png" class="mx-auto d-block" width="140" height="140">
+        <h2 style="text-align: center;">Mario Nedić</h2>
+        <p style="text-align: center;">Pozdrav, ja sam Mario Nedić. Imam 21 godinu i živim u Tolisi. Trenutno sam student treće godine Informatike u Orašju. Uz faks, slobodno vrijeme provodim s prijateljima, s bratom se bavim izradom namještaja te vikednom radim u diskoteci. 😄</p>
+        <p><a class="btn btn-primary d-grid gap-2 col-6 mx-auto" href="https://github.com/MarioNedic">Github</a></p>
       </div>
     </div>
   </div>
+
+  <hr class="featurette-divider">
+
+  <!-- Informacije -->
+  <div class="container px-4 py-5" id="featured-3">
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+      <div class="feature col">
+        <i class="bi bi-info-circle" style="font-size: 3rem; color: #0d6efd;"></i>
+        <h2>O projektu</h2>
+        <p>Svrha dokumenta je omogućiti korisnicima stranice prijavu za vakciniranje. Bilo koji korisnik će se moći prijaviti na stranicu sapotrebnim dokumentima (osobna iskaznica) te se tako prijaviti za vakciniranje u bolnici. Radni naziv projekta je „Cijepi se“ i ono također potiče ljude na cijepljenje protiv virusa.</p>
+      </div>
+      <div class="feature col">
+      <i class="bi bi-cpu" style="font-size: 3rem; color: #0d6efd;"></i>
+        <h2>Tehnologije</h2>
+        <p>Koristit ćemo tehnologije kao: Laravel, Bootstrap 5 css i javascript, MySQL.</p>
+      </div>
+      <div class="feature col">
+      <i class="bi bi-patch-question" style="font-size: 3rem; color: #0d6efd;"></i>
+        <h2>Motivacija</h2>
+        <p>Radimo ovo jer bi ova ideja jako pomogla u današnje vrijeme. Jako nas zanima kako bi mogli popraviti neke stvari u ovoj sredini, i cijepljenje je jedna od tih stvari. Ideja bi sigurno dobro prošla među našim ljudima jer olakšava cijepljenje.</p>
+      </div>
+    </div>
+  </div>
+
+
   <!-- Bootstrap JS with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+  <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
 </html>
