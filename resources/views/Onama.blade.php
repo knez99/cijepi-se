ac2476cc4295d4b832c9b88944010b45d43c7e70
unknown <david.milicic@fpmoz.sum.ba>
2022-01-16 15:49:18 +0100

    Home page novi izgled, promjena O nama

--- resources/views/Onama.blade.php
+++ resources/views/Onama.blade.php
@@ -28,9 +28,9 @@ if (!empty($_GET['q'])) {
 
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
-    <a class="navbar-brand" aria-current="page" href="{{ url('/') }}" style="font-size:30px;">Cijepi se</a>
+      <a class="navbar-brand" aria-current="page" href="{{ url('/') }}" style="font-size:30px;">Cijepi se</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -59,63 +59,34 @@ if (!empty($_GET['q'])) {
     </div>
   </nav>
 
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4">
+      <img src="images/david.png" width="140" height="140">
 
-  <!--Kartice tima-->
-  <div class="container">
-    <div class="card-group">
-      <div class="card">
-        <img src="images/david.png" class="imgcard">
-        <div class="card-body">
-          <h5 class="card-title">David Miličić</h5>
-          <p class="card-text">Pozdrav, ja sam David Miličić. Imam 21 godinu i živim u Ugljari. Student sam treće godine Informatike u Orašju. Slobodno vrijeme provodim igrajući igre i družeći se sa prijateljima.</p>
-          <p class="card-text"><a href="https://github.com/DavidMilicic">Github</a></p>
-        </div>
-      </div>
-      <div class="card">
-        <img src="images/kruno.png" class="imgcard">
-        <div class="card-body">
-          <h5 class="card-title">Krunoslav Knežević</h5>
-          <p class="card-text">Pozdrav, ja sam Krunoslav Knežević. Imam 22 godine i živim u Jenjiću. Student sam treće godine Informatike u Orašju. U slobodno vrijeme igram nogomet,igre i družim se s prijateljima.</p>
-          <p class="card-text"><a href="https://github.com/knez99">Github</a></p>
-        </div>
-      </div>
-      <div class="card">
-        <img src="images/mario.jpg" class="imgcard">
-        <div class="card-body">
-          <h5 class="card-title">Mario Nedić</h5>
-          <p class="card-text">Pozdrav, ja sam Mario Nedić. Imam 21 godinu i živim u Tolisi. Trenutno sam student treće godine Informatike u Orašju. Uz faks, slobodno vrijeme provodim s prijateljima, s bratom se bavim izradom namještaja te vikednom radim u diskoteci. 😄</p>
-          <p class="card-text"><a href="https://github.com/MarioNedic">Github</a></p>
-        </div>
-      </div>
-    </div>
-  </div>
 
-  <!--Kartice informacija-->
-  <div class="container">
-    <div class="card text-center">
-      <div class="card-body">
-        <h5 class="card-title">O projektu</h5>
-        <p class="card-text">Svrha dokumenta je omogućiti korisnicima stranice prijavu za vakciniranje. Bilo koji korisnik će se moći prijaviti na stranicu sapotrebnim dokumentima (osobna iskaznica) te se tako prijaviti za vakciniranje u bolnici. Radni naziv projekta je „Cijepi se“ i ono također potiče ljude na cijepljenje protiv virusa.</p>
+        <h2>David Miličić<</h2>
+        <p>Pozdrav, ja sam David Miličić. Imam 21 godinu i živim u Ugljari. Student sam treće godine Informatike u Orašju. Slobodno vrijeme provodim igrajući igre i družeći se sa prijateljima.</p>
+        <p><a class="btn btn-primary" href="#">Github</a></p>
       </div>
-    </div>
-  </div>
-  <div class="container">
-    <div class="card text-center">
-      <div class="card-body">
-        <h5 class="card-title">Tehnologije</h5>
-        <p class="card-text">Koristit ćemo tehnologije kao: Laravel, Bootstrap 5 css i javascript, MySQL.</p>
-      </div>
-    </div>
-  </div>
-  <div class="container">
-    <div class="card text-center">
-      <div class="card-body">
-        <h5 class="card-title">Motivacija</h5>
-        <p class="card-text">Radimo ovo jer bi ova ideja jako pomogla u današnje vrijeme. Jako nas zanima kako bi mogli popraviti neke stvari u ovoj sredini, i cijepljenje je jedna od tih stvari. Ideja bi sigurno dobro prošla među našim ljudima jer olakšava cijepljenje.</p>
-      </div>
-    </div>
-  </div>
+      <div class="col-lg-4">
+        <img src="images/kruno.png" width="140" height="140">
 
+
+        <h2>Krunoslav Knežević</h2>
+        <p>Pozdrav, ja sam Krunoslav Knežević. Imam 22 godine i živim u Jenjiću. Student sam treće godine Informatike u Orašju. U slobodno vrijeme igram nogomet,igre i družim se s prijateljima.</p>
+        <p><a class="btn btn-primary" href="#">Github</a></p>
+      </div>
+      <div class="col-lg-4">
+      <img src="images/mario.png" width="140" height="140">
+
+
+        <h2>Mario Nedić</h2>
+        <p>Pozdrav, ja sam Mario Nedić. Imam 21 godinu i živim u Tolisi. Trenutno sam student treće godine Informatike u Orašju. Uz faks, slobodno vrijeme provodim s prijateljima, s bratom se bavim izradom namještaja te vikednom radim u diskoteci. 😄</p>
+        <p><a class="btn btn-primary" href="#">Github</a></p>
+      </div>
+    </div>
+  </div>
   <!-- Bootstrap JS with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,44 +27,105 @@ if (!empty($_GET['q'])) {
 </head>
 
 <body>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container-fluid">
+        <a class="navbar-brand" aria-current="page" href="{{ url('/') }}" style="font-size:30px;">Cijepi se</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" href="{{ url('/Onama') }}">O nama</a>
+            </li>
+            <li> @if (Route::has('login'))
+              @auth
+              <!-- Ovaj home maknit ne treba???? -->
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div class="container-fluid">
-      <a class="navbar-brand" aria-current="page" href="{{ url('/') }}" style="font-size:30px;">Cijepi se</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" href="{{ url('/Onama') }}">O nama</a>
-          </li>
-          <li> @if (Route::has('login'))
-            @auth
-            <a class="nav-link active" href="{{ url('/home') }}">Home</a>
-          </li>
-          <li>
-            @else
-            <a class="nav-link active" href="{{ route('login') }}">Log in</a>
-          </li>
-          <li>
-            @if (Route::has('register'))
-            <a class="nav-link active" href="{{ route('register') }}">Register</a>
-            @endif
-            @endauth
-            @endif
-          </li>
-        </ul>
+            </li>
+            <li>
+              @else
+              <a class="nav-link active" href="{{ route('login') }}">Log in</a>
+            </li>
+            <li>
+              @if (Route::has('register'))
+              <a class="nav-link active" href="{{ route('register') }}">Register</a>
+              @endif
+              @endauth
+              @endif
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </header>
 
-<div class="d-flex justify-content-center mx-auto">
-<img class="img-fluid" src="images/covid19.jpg">
-</div>
+
+  <main>
+
+    <div class="container">
+
+
+      <!-- Featurettes -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Što je cjepivo?</h2>
+          <p class="lead">Cjepivo je vrsta biološkog preparata koja poboljšava odnosno pomaže stvaranju otpornosti prema nekoj bolesti. U cjepivu se obično nalaze tvari koje predstavljaju neku bolest, a te tvari mogu biti: oslabljeni ili mrtvi mikroorganizmi koji uzrokuju neku bolest, toksini ili dijelovi proteina omotača mikroorganizama ili RNK mikroorganizama. U organizam se mogu unositi injekcijom u kožu, pod kožu ili u mišić, u usta ili u nos.</p>
+        </div>
+        <div class="col-md-5">
+        <img src="images/Upitnik.png" class="img-fluid">
+
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 order-md-2">
+          <h2 class="featurette-heading">Zašto se cijepiti?</h2>
+          <p class="lead">Najprije cijepljenje nam pruža imunitet: pruža dugotrajnu, ponekad doživotnu zaštitu od bolesti. Cjepiva koja se preporučuju u programu cijepljenja u ranoj dječjoj dobi štite djecu od ospica, vodenih kozica i drugih bolesti. Sekundarna korist cijepljenja je "imunitet stada", također poznat kao imunitet zajednice. Imunitet stada nam svima pruža zaštitu kako je veći broj cijepljenih u zajednici. Uz dovoljno ljudi s imunitetom protiv određene bolesti, teško je da se bolest pojavi u zajednici.</p>
+        </div>
+        <div class="col-md-5 order-md-1">
+        <img src="images/ZastoCijepiti.png" class="img-fluid">
+
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Kako se prijaviti za cijepljenje?</span></h2>
+          <p class="lead">Jednostavno se prijavite na stranicu, ispunite odgovarajuća polja te pogledajte dostupne datume za cijepljenje!</p>
+        </div>
+        <div class="col-md-5">
+        <img src="images/KakoPrijaviti.png" class="img-fluid">
+
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <!-- End of Featurettes -->
+
+    </div>
+
+  </main>
+
+
+  <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
   <!-- Bootstrap JS with Popper -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+
+
+
 
 </body>
 
